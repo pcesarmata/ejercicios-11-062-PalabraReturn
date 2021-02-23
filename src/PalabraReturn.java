e9efe4cd8514a75f78cac0acf5bbba30b1c2514c
Cesa <pcesarmata@gmail.com>
2021-02-22 21:55:45 -0600

    init commit

---
+++ src/PalabraReturn.java
@@ -0,0 +1,10 @@
+public class PalabraReturn {
+    public static void main(String[] args) {
+        int resultado = sumar(3, 6);
+        System.out.println("resultado = " + resultado);
+    }
+
+    public static int sumar(int a , int b){
+        return a + b;
+    }
+}
